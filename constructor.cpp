db1a070e3a69c7d120a60bc8e6bc9ce99182920f
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-05-04 19:15:35 +0700

    class mahasiswa constructor

--- constructor.cpp
+++ constructor.cpp
@@ -6,11 +6,3 @@ public:
     mahasiswa(); // Constructor
 };
 
-mahasiswa::mahasiswa() {
-    cout << "Constructor Terpanggil" << endl; // Definisi Constructor
-};
-
-int main() {
-    mahasiswa mhs; // Pembuatan Object
-    return 0;
-}
